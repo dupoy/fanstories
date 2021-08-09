4d801c4a3909077a06c2bba96cdab968f0b1ba08
Bilash Denis <faaakiring@gmail.com>
2021-08-09 12:26:23 +0300

    orm config

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/orm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
