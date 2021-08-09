7c0455fb6aebb6881cbc561fac32b5eb09ff0742
Bilash Denis <faaakiring@gmail.com>
2021-08-09 17:05:05 +0300

    login & register endpoint

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { IUserResponse } from './types/userResponse.interface';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller()
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/auth/register')
+  async register(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<IUserResponse> {
+    return this.userService.buildResponse(
+      await this.userService.createUser(createUserDto),
+    );
+  }
+
+  @Post('/auth/login')
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<IUserResponse> {
+    return this.userService.buildResponse(
+      await this.userService.loginUser(loginUserDto),
+    );
+  }
+}
--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,7 +1,12 @@
 import { UserEntity } from './../../entities/user.entity';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/createUser.dto';
+import { IUserResponse } from './types/userResponse.interface';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
+import { LoginUserDto } from './dto/loginUser.dto';
 
 @Injectable()
 export class UserService {
@@ -10,7 +15,71 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  async findById(currentUserId: number) {
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    let condidate = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+
+    if (condidate) {
+      throw new HttpException(
+        'User with this email is already exist',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    condidate = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+
+    if (condidate) {
+      throw new HttpException(
+        'User with this username is already exist',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async loginUser(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const condidate = await this.userRepository.findOne({
+      email: loginUserDto.email,
+    });
+
+    if (!condidate) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const isPasswordMatch = await compare(
+      loginUserDto.password,
+      condidate.password,
+    );
+
+    if (!isPasswordMatch) {
+      throw new HttpException(
+        'Password mismatch',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    return condidate;
+  }
+
+  async findById(currentUserId: number): Promise<UserEntity> {
     return this.userRepository.findOne(currentUserId);
+  }
+
+  buildResponse(user: UserEntity): IUserResponse {
+    delete user.password;
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
+  }
+
+  private generateJwt({ id, username, email }: UserEntity): string {
+    return sign({ id, username, email }, process.env.JWT_SECRET);
   }
 }
